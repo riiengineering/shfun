d2061c62670a41564ef6cdd9cd7a4f583e5a2aca
Dennis Camera <dennis.camera@riiengineering.ch>
2023-10-02 14:25:03 +0200

    [quote/shquot] Add usage comment

--- lib/quote/shquot.sh
+++ lib/quote/shquot.sh
@@ -1,3 +1,9 @@
+# Quotes all arguments into a single "shell" string.
+#
+# Usage:
+#  - quote foo bar baz
+#  - quote 'foo bar baz'
+
 sed -e "s/'/'\\\\''/g" -e "1s/^/'/" -e "\$s/\$/'/" <<EOF
 $*
 EOF
