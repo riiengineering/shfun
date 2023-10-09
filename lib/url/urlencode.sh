6adcc172e6ef0f1ad4622deacb35e85b149ea215
Dennis Camera <dennis.camera@riiengineering.ch>
2023-10-09 11:14:08 +0200

    [url/urlencode] Use regex to find characters to encode

--- lib/url/urlencode.sh
+++ lib/url/urlencode.sh
@@ -3,17 +3,25 @@
 LC_ALL=C awk '
 BEGIN {
 	# pre-calculate lookup table
-	for (i = 1;i < 256; ++i)
+	for (i = 1;i <= 47; ++i)
+		ord[sprintf("%c", i)] = i
+	for (i = 58;i <= 64; ++i)
+		ord[sprintf("%c", i)] = i
+	for (i = 91;i <= 96; ++i)
+		ord[sprintf("%c", i)] = i
+	for (i = 123;i < 256; ++i)
 		ord[sprintf("%c", i)] = i
 }
 NR > 1 {
 	printf "%%%02X", ord[ORS]
 }
 {
-	for(i = 1; i <= length; ++i) {
-		c = substr($0, i, 1)
-		printf "%s", (c ~ /[0-9A-Za-z.~_-]/) ? c : sprintf("%%%02X", ord[c])
+	l = $0
+	while (match(l, /[^0-9A-Za-z.~_-]/)) {
+		printf "%s%%%02X", substr(l, 1, RSTART-1), ord[substr(l, RSTART, 1)]
+		l = substr(l, RSTART + 1)
 	}
+	printf "%s", l
 }
 END {
 	printf "%s", ORS
