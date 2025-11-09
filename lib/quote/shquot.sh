ba9e7fae4501c71ee87f6191e8ad4bf2fc1f088f
Dennis Camera <dennis.camera@riiengineering.ch>
2025-11-09 20:49:14 +0100

    [quote/shquot] Fix name in usage comment

--- lib/quote/shquot.sh
+++ lib/quote/shquot.sh
@@ -1,8 +1,8 @@
 # Quotes all arguments into a single "shell" string.
 #
 # Usage:
-#  - quote foo bar baz
-#  - quote 'foo bar baz'
+#  - shquot foo bar baz
+#  - shquot 'foo bar baz'
 
 sed -e "s/'/'\\\\''/g" -e "1s/^/'/" -e "\$s/\$/'/" <<EOF
 $*
